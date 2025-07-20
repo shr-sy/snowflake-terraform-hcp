1a01b9e51056b88be06ca41e7839acc6ef05a584
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-20 12:33:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,37 @@
-variable "snowflake_account_locator" {
-  description = "Account locator only, e.g., xy12345"
+variable "snowflake_account_name" {
   type        = string
+  description = "Snowflake account name (e.g., DW)"
 }
 
-variable "snowflake_user" {
-  description = "Snowflake user name"
+variable "snowflake_organization_name" {
   type        = string
+  description = "Snowflake organization name (e.g., TEMPERPACK)"
+}
+
+variable "snowflake_username" {
+  type        = string
+  description = "Username for Snowflake access"
 }
 
 variable "snowflake_password" {
-  description = "Snowflake user password"
   type        = string
+  description = "Password for Snowflake user"
   sensitive   = true
 }
 
 variable "snowflake_role" {
-  description = "Snowflake role"
   type        = string
+  description = "Snowflake role for Terraform actions"
+}
+
+variable "snowflake_region" {
+  type        = string
+  description = "Snowflake region (e.g., us-west-2)"
+}
+providers.tf
+provider "snowflake" {
+  account            = "${var.snowflake_organization_name}.${var.snowflake_account_name}"
+  user               = var.snowflake_username
+  password           = var.snowflake_password
+  role               = var.snowflake_role
 }
