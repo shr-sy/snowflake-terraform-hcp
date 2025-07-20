c77ef43ad31c08d4b3244cd3f0deb773a7de7780
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-20 13:34:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,11 +7,13 @@ terraform {
   }
 }
 
+locals {
+  full_snowflake_account = "${var.snowflake_account_name}.${var.snowflake_region}"
+}
+
 provider "snowflake" {
-  account_name       = var.snowflake_account_name
-  organization_name  = var.snowflake_organization_name
-  region             = var.snowflake_region
-  user               = var.snowflake_username
-  password           = var.snowflake_password
-  role               = var.snowflake_role
+  account  = local.full_snowflake_account
+  user     = var.snowflake_username
+  password = var.snowflake_password
+  role     = var.snowflake_role
 }
