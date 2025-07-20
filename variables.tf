39716538dddc6ffab77e1777352238fb1cad2621
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-20 13:37:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variable "snowflake_account_name" {
   type        = string
-  description = "Snowflake account name (e.g., xy12345)"
+  description = "Account locator prefix (e.g., xy12345)"
 }
 
 variable "snowflake_region" {
   type        = string
-  description = "Snowflake region (e.g., central-india.azure)"
+  description = "Region locator suffix (e.g., central-india.azure)"
 }
 
 variable "snowflake_username" {
   type        = string
-  description = "Username for Snowflake access"
+  description = "Username for Snowflake"
 }
 
 variable "snowflake_password" {
   type        = string
-  description = "Password for Snowflake user"
+  description = "Password for Snowflake"
   sensitive   = true
 }
 
 variable "snowflake_role" {
   type        = string
-  description = "Snowflake role for Terraform actions"
+  description = "Snowflake role"
 }
