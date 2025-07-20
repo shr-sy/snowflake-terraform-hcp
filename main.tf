4986c59fb57dc92e348f0eba89426d52d1b30b61
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-20 12:34:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@ resource "snowflake_warehouse" "test_wh" {
   name                          = "TEST_WH"
   warehouse_size                = "XSMALL"
   auto_suspend                  = 60
+  auto_resume                   = false
   initially_suspended           = true
-  comment                       = "Test warehouse created by Terraform"
+  comment                       = "Testing warehouse"
+  max_cluster_count             = 2
+  min_cluster_count             = 1
+  scaling_policy                = "STANDARD"
+  statement_timeout_in_seconds = 300
 }
+#resource "snowflake_warehouse_grant" "test_wh_ownership" {
+#  warehouse_name     = snowflake_warehouse.test_wh.name
+#  privilege          = "OWNERSHIP"
+#  roles              = ["DBA"]
+#  with_grant_option  = true
+
+#  depends_on = [snowflake_warehouse.test_wh]
+#}
